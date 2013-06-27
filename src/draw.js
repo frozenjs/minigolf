6f6c058f50445cbff84f825bf13ce66e12cb6ecc
Blaine Bublitz <blaine@iceddev.com>
2013-06-26 18:10:36 -0700

    remove state, refactor some code

--- src/draw.js
+++ src/draw.js
@@ -1,15 +1,13 @@
 define([
-  './state',
   './levelData',
   './rotateVector',
-  './getHoleScoreName',
   'lodash',
   'dojo/has',
   'frozen/utils/distance',
   'frozen/utils/scalePoints',
   'frozen/utils/translatePoints',
   'frozen/utils/radiansFromCenter'
-], function(state, levelData, rotateVector, getHoleScoreName, _, has, distance, scalePoints, translatePoints, radiansFromCenter){
+], function(levelData, rotateVector, _, has, distance, scalePoints, translatePoints, radiansFromCenter){
 
   'use strict';
 
@@ -21,21 +19,22 @@ define([
 
   return function(context){
     var ball = this.entities.ball;
+    var im = this.inputManager;
 
     context.lineWidth = 1;
-    context.drawImage(levelData[state.level].backImg, 0, 0, this.width, this.height);
+    context.drawImage(levelData[this.level].backImg, 0, 0, this.width, this.height);
     ball.draw(context);
 
-    if(has('debug')){
-      _.forEach(this.entities, function(entity){
+    _.forEach(this.entities, function(entity){
+      if(!entity.staticBody || has('debug')){
         entity.draw(context);
-      });
-    }
+      }
+    });
 
-    if(state.swingPt){
+    if(im.mouseAction.position){
       var ballPt = scalePoints(ball, ball.scale);
-      var impPerc = (Math.min(distance(ballPt, state.swingPt), MAX_IMPULSE_PIXELS)) / MAX_IMPULSE_PIXELS;
-      var angle = radiansFromCenter(ballPt, state.swingPt) + Math.PI;
+      var impPerc = (Math.min(distance(ballPt, im.mouseAction.position), MAX_IMPULSE_PIXELS)) / MAX_IMPULSE_PIXELS;
+      var angle = radiansFromCenter(ballPt, im.mouseAction.position) + Math.PI;
       var rotated = rotateVector({x: 0, y: impPerc * MAX_IMPULSE_PIXELS }, angle);
       var p2 = translatePoints(rotated, ballPt);
       var ogLineWidth = context.lineWidth;
@@ -53,7 +52,7 @@ define([
       context.lineWidth = ogLineWidth;
     }
 
-    this.ui.draw(context);
+    this.ui.draw(context, this.level);
   };
 
 });
