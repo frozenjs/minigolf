575aff6ffe6fd94c8a5ee001cc8b4ca4d4b69975
Blaine Bublitz <blaine@iceddev.com>
2013-07-17 18:35:25 -0700

    break ffos stuff into own modules

--- dojoConfig.js
+++ dojoConfig.js
@@ -5,9 +5,11 @@ var dojoConfig = {
     { name: 'dcl', location: 'deps/dcl', main: 'dcl' },
     { name: 'lodash', location: 'deps/lodash/dist', main: 'lodash' },
     { name: 'hammer', location: 'deps/hammer', main: 'hammer' },
+    { name: 'firefox-install', location: 'deps/firefox-install' },
+    { name: 'firefox-landscape', location: 'deps/firefox-landscape' },
     { name: 'frozen', location: 'deps/frozen/src', main: 'GameCore' },
     { name: 'game', location: 'src', main: 'game' }
   ],
-  deps: ['game'],
+  deps: ['firefox-install', 'firefox-landscape', 'game'],
   async: true
 };
